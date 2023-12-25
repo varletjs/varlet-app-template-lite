15b397fc2dcde80305c25e5d39010a7db6ee3217
haoziqaq <357229046@qq.com>
2023-12-25 14:59:45 +0800

    fix: install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import App from './App.vue'
 import router from '@/router'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import '@/styles/common.less'
 import '@varlet/touch-emulator'
 import 'virtual:uno.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
--- src/store/global.ts
+++ src/store/global.ts
@@ -1,5 +1,3 @@
-export const useGlobalStore = defineStore('global', {
-  state: () => ({}),
-  getters: {},
-  actions: {}
+export const useGlobalStore = defineStore('global', () => {
+  return {}
 })
