06a977a692b0e7c6c93cbea65c3e9cd277264da1
haoziqaq <357229046@qq.com>
2024-03-22 01:40:03 +0800

    feat: update

--- uno.config.ts
+++ uno.config.ts
@@ -4,6 +4,13 @@ import presetRemToPx from '@unocss/preset-rem-to-px'
 
 export default defineConfig({
   presets: [presetUno(), presetRemToPx(), presetIcons(), presetVarlet()],
+  theme: {
+    colors: {
+      secondary: 'var(--color-secondary)',
+      tertiary: 'var(--color-tertiary)',
+      muted: 'var(--color-muted)'
+    }
+  },
   rules: [
     [
       /^text-(.*)$/,
@@ -12,7 +19,12 @@ export default defineConfig({
           xl: '18px',
           '2xl': '20px',
           '3xl': '22px',
-          '4xl': '24px'
+          '4xl': '24px',
+          '5xl': '26px',
+          '6xl': '28px',
+          '7xl': '30px',
+          '8xl': '32px',
+          '9xl': '34px'
         }
 
         return {
