c2ec9e0dc1349a6b853f1167f942c918396711e1
haoziqaq <357229046@qq.com>
2024-03-22 01:42:01 +0800

    feat: update

--- src/pages/layout.vue
+++ src/pages/layout.vue
@@ -5,7 +5,7 @@ const { toggleDark } = useDark()
 </script>
 
 <template>
-  <main class="flex min-h-screen justify-center items-center">
+  <div class="container flex min-h-screen justify-center items-center mx-auto">
     <var-button type="primary" @click="toggleDark">Toggle Theme</var-button>
-  </main>
+  </div>
 </template>
