0fc49373d962c6453dc182f5ba2043ff0b727f7a
haoziqaq <357229046@qq.com>
2024-05-11 18:10:16 +0800

    chore: bump

--- src/use/useDark.ts
+++ src/use/useDark.ts
@@ -7,13 +7,13 @@ export function useDark() {
   const isDark = ref(saved || (saved == null && window.matchMedia('(prefers-color-scheme: dark)').matches))
 
   function updateTheme() {
+    localStorage.set('prefer-dark', isDark.value)
     StyleProvider(isDark.value ? darkTheme : lightTheme)
     document.documentElement.style.setProperty('color-scheme', isDark.value ? 'dark' : 'light')
   }
 
   function toggleDark() {
     isDark.value = !isDark.value
-    localStorage.set('prefer-dark', isDark.value)
     updateTheme()
   }
 
